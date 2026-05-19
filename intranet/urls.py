5538317d16deb8c97d967bf1fe457605bdb18e01
Анатолий <parshikov.anatoliy@list.ru>
2026-05-19 13:35:48 +0700

    Serve media files on Railway

--- intranet/urls.py
+++ intranet/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,6 +14,7 @@ urlpatterns = [
     path("tasks/", include("tasks.urls")),
     path("chat/", include("chat.urls")),
     path("notifications/", include("notifications.urls")),
-]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # media files on Railway
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
